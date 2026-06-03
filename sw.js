fff2f7bea82bbb29341b47cfed54c28f72f8b2af
Saravanan <test@example.com>
2026-06-03 20:06:58 +0530

    Deploy build 128 - enlarge app popup modals ~2x

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LifeOS Service Worker - v127.0
-const CACHE = 'lifeos-v127';
+// LifeOS Service Worker - v128.0
+const CACHE = 'lifeos-v128';
 
 // Allow the page to tell a waiting SW to activate immediately
 self.addEventListener('message', e => {
