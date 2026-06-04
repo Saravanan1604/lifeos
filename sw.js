e2b188e5e23c687b1af3abd24ce7c1c8f8b34ad6
Saravanan <test@example.com>
2026-06-04 07:32:39 +0530

    Deploy build 179 - Samsung-style keypad

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LifeOS Service Worker - v178.0
-const CACHE = 'lifeos-v178';
+// LifeOS Service Worker - v179.0
+const CACHE = 'lifeos-v179';
 
 // Allow the page to tell a waiting SW to activate immediately
 self.addEventListener('message', e => {
