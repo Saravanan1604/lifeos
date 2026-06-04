354254452aa10ae5806eff0b716a60c63989df40
Saravanan <test@example.com>
2026-06-04 06:19:02 +0530

    Deploy build 166 - keypad fills bottom evenly

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LifeOS Service Worker - v165.0
-const CACHE = 'lifeos-v165';
+// LifeOS Service Worker - v166.0
+const CACHE = 'lifeos-v166';
 
 // Allow the page to tell a waiting SW to activate immediately
 self.addEventListener('message', e => {
