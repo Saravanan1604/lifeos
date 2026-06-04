5e4a9cfa6f703eeca0b5bea5692c1cc62e4fbd6d
Saravanan <test@example.com>
2026-06-04 06:34:42 +0530

    Deploy build 171 - bigger calculator keys

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LifeOS Service Worker - v170.0
-const CACHE = 'lifeos-v170';
+// LifeOS Service Worker - v171.0
+const CACHE = 'lifeos-v171';
 
 // Allow the page to tell a waiting SW to activate immediately
 self.addEventListener('message', e => {
