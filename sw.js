77bb9650cc7d78613448f01b7797e6ef6bf6303a
Saravanan <test@example.com>
2026-06-04 23:34:17 +0530

    Deploy build 251

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LifeOS Service Worker - v250.0
-const CACHE = 'lifeos-v250';
+// LifeOS Service Worker - v251.0
+const CACHE = 'lifeos-v251';
 
 // Allow the page to tell a waiting SW to activate immediately
 self.addEventListener('message', e => {
