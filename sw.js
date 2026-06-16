d05f298abd922f149974873bfed5c28e21646498
Saravanan <test@example.com>
2026-06-16 07:31:09 +0530

    Deploy build 507

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// atworth Service Worker - v506.0
-const CACHE = 'lifeos-v506';
+// atworth Service Worker - v507.0
+const CACHE = 'lifeos-v507';
 
 // Allow the page to tell a waiting SW to activate immediately
 self.addEventListener('message', e => {
