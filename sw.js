10035633410814b64d2398e0b373b020df697cf4
Saravanan <test@example.com>
2026-06-25 13:47:28 +0530

    Deploy build 626: group txpad calc+mic equal-size, kill notes dead-space

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// atworth Service Worker - v625.0
-const CACHE = 'lifeos-v625';
+// atworth Service Worker - v626.0
+const CACHE = 'lifeos-v626';
 
 // Allow the page to tell a waiting SW to activate immediately
 self.addEventListener('message', e => {
